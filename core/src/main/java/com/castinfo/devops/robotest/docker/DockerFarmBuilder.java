63c989ba4c42be007c6afdb97f35ba3de49c0e0c
Miguel David Ruiz Caparrós <miguel.ruiz@cast-info.es>
2018-06-05 00:44:53 +0200

    redefine bind ports

--- core/src/main/java/com/castinfo/devops/robotest/docker/DockerFarmBuilder.java
+++ core/src/main/java/com/castinfo/devops/robotest/docker/DockerFarmBuilder.java
@@ -21,6 +21,7 @@ import java.io.IOException;
 import java.net.URI;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.concurrent.atomic.AtomicInteger;
 
@@ -37,6 +38,8 @@ import com.github.dockerjava.api.command.CreateContainerResponse;
 import com.github.dockerjava.api.command.InspectContainerResponse;
 import com.github.dockerjava.api.model.Bind;
 import com.github.dockerjava.api.model.ExposedPort;
+import com.github.dockerjava.api.model.Ports;
+import com.github.dockerjava.api.model.Ports.Binding;
 import com.github.dockerjava.api.model.Volume;
 import com.github.dockerjava.core.DefaultDockerClientConfig;
 import com.github.dockerjava.core.DefaultDockerClientConfig.Builder;
@@ -251,11 +254,18 @@ public class DockerFarmBuilder {
             containerCmd.withName(dockerBrowser.getContainerName());
             containerCmd.withLabels(dockerBrowser.getLabels());
             containerCmd.withNetworkMode(dockerBrowser.getNetworkMode());
+            Ports portBindings = new Ports();
+            int exposedPort = Integer.parseInt(dockerBrowser.getExposePort());
+            ExposedPort exposePort = ExposedPort.tcp(exposedPort);
+            containerCmd.withExposedPorts(exposePort);
+            portBindings.bind(exposePort, Binding.bindPort(exposedPort));
             if (StringUtils.isNotEmpty(dockerBrowser.getExposeDebugPort())) {
-                containerCmd.withExposedPorts(ExposedPort.tcp(Integer.parseInt(dockerBrowser.getExposeDebugPort())));
+                exposedPort = Integer.parseInt(dockerBrowser.getExposeDebugPort());
+                exposePort = ExposedPort.tcp(exposedPort);
+                containerCmd.withExposedPorts(exposePort);
+                portBindings.bind(exposePort, Binding.bindPort(exposedPort));
             }
-            containerCmd.withExposedPorts(ExposedPort.tcp(Integer.parseInt(dockerBrowser.getExposePort())));
-            containerCmd.withPublishAllPorts(true);
+            containerCmd.withPortBindings(portBindings);
             Volume sharedMemory = new Volume("/dev/shm");
             containerCmd.withVolumes(sharedMemory).withBinds(new Bind("/dev/shm", sharedMemory));
             containerResp = containerCmd.exec();

19107d9da7f83510ff7d550ecac632219219f870
Miguel David Ruiz Caparrós <miguel.ruiz@cast-info.es>
2018-06-06 14:57:51 +0200

    proxy settings

--- core/src/main/java/com/castinfo/devops/robotest/selenium/SeleniumDriverFactory.java
+++ core/src/main/java/com/castinfo/devops/robotest/selenium/SeleniumDriverFactory.java
@@ -29,7 +29,6 @@ import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.tuple.ImmutablePair;
 import org.apache.commons.lang3.tuple.Pair;
 import org.apache.http.ssl.SSLInitializationException;
-import org.openqa.selenium.Proxy;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebDriverException;
 import org.openqa.selenium.chrome.ChromeDriver;

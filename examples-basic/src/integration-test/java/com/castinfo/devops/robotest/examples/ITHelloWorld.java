3ec46c3190325e6b4735eb38a4d3d1a0e4f88f67
Miguel David Ruiz Caparrós <miguel.ruiz@cast-info.es>
2018-06-16 22:01:17 +0200

    test proxy

--- examples-basic/src/integration-test/java/com/castinfo/devops/robotest/examples/ITHelloWorld.java
+++ examples-basic/src/integration-test/java/com/castinfo/devops/robotest/examples/ITHelloWorld.java
@@ -26,6 +26,7 @@ public class ITHelloWorld extends TestCase {
         HelloWorldPageObject helloWorldPO = this.buildPageObject(HelloWorldPageObject.class);
         if ("CHROME".equalsIgnoreCase(System.getProperty("ROBOTEST_BROWSER"))) {
             helloWorldPO.openURL("chrome://net-internals/proxyservice#proxy");
+            helloWorldPO.noOpCode();
         }
         String urlToTest = this.getSuiteTestPropertyCfg("WEB_TO_TEST", "CAST-INFO-WEB");
         helloWorldPO.addInfoToReport().withMessage("Try to open:" + urlToTest);
